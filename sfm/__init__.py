0d9013a784981bdc964ab2be5ed8ccf97d83212c
Minhyeok Im <minhyeok0104@gmail.com>
2025-08-05 14:15:08 +0900

    package name update

--- sfm/__init__.py
+++ sfm/__init__.py
@@ -2,4 +2,19 @@
 Enhanced Structure-from-Motion Package
 """
 
-__version__ = "0.1.0" 
+__version__ = "0.1.0"
+
+# Import the main classes and functions
+from ..enhanced_sfm import (
+    EnhancedSfM,
+    quick_sfm,
+    high_quality_sfm,
+    fast_sfm
+)
+
+__all__ = [
+    "EnhancedSfM",
+    "quick_sfm", 
+    "high_quality_sfm",
+    "fast_sfm"
+] 
